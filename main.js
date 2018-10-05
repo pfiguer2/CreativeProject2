0cf7bb496ed96b8ff75a06fd141ecb4c363692e1
Danni Figueroa <peaches21093@gmail.com>
2018-10-05 02:09:09 +0000

    more words

--- main.js
+++ main.js
@@ -3,38 +3,38 @@ function randomGenerator(min, max) {
 }
 
 function trendyDad() {
-  var options1 = ["sneakers", "cat", "design", "glasses", "hat"];
+  var options1 = ["sneakers", "cat", "design", "glasses", "hat", "hipster", "suit", "shoes", "glitter"];
   var randInt = randomGenerator(0, options1.length - 1);
   var itemTrendyDad = options1[randInt];
   
 }
 
 function nottrendyDad() {
-  var options2 = [];
+  var options2 = ["socks", "beer", "pickles", "baseball", "hotdogs", "screwdriver", "duck", "barbies"];
   var randInt = randomGenerator(0, options2.length - 1);
   var itemNotTrendyDad = options2[randInt];
 }
 
 function handymanDad() {
-  var options3 = [];
+  var options3 = ["hammer", "toolbelt", "tools", "mistakes", "broken", "fix"];
   var randInt = randomGenerator(0, options3.length - 1);
   var itemHandymanDad = options3[randInt];
 }
 
 function chefDad() {
-  var options4 = [];
+  var options4 = ["cooking", "food", "cheese", "wine", "poodle"];
   var randInt = randomGenerator(0, options4.length - 1);
   var itemChefDad = options4[randInt];
 }
 
 function chuloDad() {
-  var options5 = [];
+  var options5 = ["exercise", "muscles", "protein", "drugs", "bear"];
   var randInt = randomGenerator(0, options5.length - 1);
   var itemChuloDad = options5[randInt];
 }
 
 function gangsterDad() {
-  var options6 = [];
+  var options6 = ["nice", "ghetto"];
   var randInt = randomGenerator(0, options6.length - 1);
   var itemGangsterDad = options6[randInt];
 }
